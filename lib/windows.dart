006624a534a036650fa9ec3dea52869c31f5b678
qi-xmu <360141773@qq.com>
2023-12-01 15:39:01 +0800

    update build script

--- lib/windows.dart
+++ lib/windows.dart
@@ -1,20 +1,26 @@
+import 'dart:io';
+
 import 'package:flutter/widgets.dart';
 import 'package:window_manager/window_manager.dart';
 
 void windowAlwaysOnTop(bool state) async {
-  await windowManager.setAlwaysOnTop(state);
+  if (Platform.isLinux || Platform.isWindows || Platform.isMacOS) {
+    await windowManager.setAlwaysOnTop(state);
+  }
 }
 
 void initWindowsSize() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isLinux || Platform.isWindows || Platform.isMacOS) {
+    WidgetsFlutterBinding.ensureInitialized();
 
-  await windowManager.ensureInitialized();
-  WindowOptions windowOptions = const WindowOptions(
-    size: Size(360, 600),
-  );
-  windowManager.waitUntilReadyToShow(windowOptions, () async {
-    await windowManager.setAlwaysOnTop(true);
-    await windowManager.setTitle("Spark AI Tools");
-    await windowManager.show();
-  });
+    await windowManager.ensureInitialized();
+    WindowOptions windowOptions = const WindowOptions(
+      size: Size(360, 600),
+    );
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.setAlwaysOnTop(true);
+      await windowManager.setTitle("Spark AI Tools");
+      await windowManager.show();
+    });
+  }
 }
