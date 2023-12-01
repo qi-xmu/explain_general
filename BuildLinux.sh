5f53db697fbe9f3ff644d64dcadc4b0eda5b2872
qi-xmu <360141773@qq.com>
2023-12-01 16:33:07 +0800

    update scripts

--- BuildLinux.sh
+++ BuildLinux.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 version=-$1
 APP=build/linux/x64/release/bundle
-if [ ! -d "$APP" ]; then
-    flutter build linux --release
-fi
+
+flutter build linux --release
+
 mv $APP explain_general
 zip -r explain_general-linux-x64$version.zip explain_general
 rm -rf explain_general
--- BuildMacos.sh
+++ BuildMacos.sh
@@ -1,9 +1,9 @@
 #! /bin/zsh
 version=-$1
 APP=build/macos/Build/Products/Release/explain_general.app
-if [ ! -d "$APP" ]; then
-    flutter build macos --release
-fi
+
+flutter build macos --release
+
 mv $APP ./SparkAI.app
 zip -r explain_general-macos-amr64$version.zip SparkAI.app
 rm -rf SparkAI.app
